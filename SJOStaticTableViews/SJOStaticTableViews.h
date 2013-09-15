d0b96c3c42b434290b96710cfb784935ac3529e9
Sam Oakley <sam@blork.co.uk>
2013-09-15 23:52:14 +0100

    Importable header file.

---
+++ SJOStaticTableViews/SJOStaticTableViews.h
@@ -0,0 +1,11 @@
+//
+//  SJOStaticTableViews.h
+//  Pinner
+//
+//  Created by Sam Oakley on 26/08/2013.
+//  Copyright (c) 2013 Sam Oakley. All rights reserved.
+//
+
+#import "SJOStaticTableViewDataSource.h"
+#import "SJOStaticCellData.h"
+#import "SJOStaticSection.h"
